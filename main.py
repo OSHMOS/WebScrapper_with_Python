bce1fde456861c9567d24bad29715d6f47154012
OSHMOS <ojames9707@gmail.com>
2022-04-18 12:15:00 +0900

    Built in Functions : Python has many functions

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
-# immutable sequence
-# tuple
-# save not only string but also many data types
-days = ("Mon", "Tue", "Wed", "Thur", "Fri")
+# functions
+print(len("aslfj;klasdjfljdsa"))
 
-print(type(days))
-
-# dictionary
-# save even list and tuple
-oshmos = {
-    'name': 'oshmos',
-    'age': 24,
-    'korean': True,
-    'fav_food': ['chicken']
-}
-print(oshmos)
-oshmos['handsome'] = True
-print(oshmos)
+age = "18"
+print(type(age))
+n_age = int(age)
+print(type(n_age))
