1f539a1c1cd72b96521bb381a1f1f51c2a9f4832
OSHMOS <ojames9707@gmail.com>
2022-04-19 10:35:48 +0900

    Function Arguments

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 # functions
-def say_hello():
-    print('hello')
+def say_hello(who = 'anonymous'):
+    print('hello', who)
 
 # () is functions' button
 say_hello()
+say_hello('oshmos')
+
+def plus(a, b):
+    print(a + b)
+
+plus(2, 5)
