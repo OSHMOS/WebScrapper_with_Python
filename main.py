fc3c2ad25128c5d95cac004e721687205e7793b2
OSHMOS <ojames9707@gmail.com>
2022-04-20 14:50:20 +0900

    Conditionals : if True: else False:

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
+# if else
 def plus(a, b):
-    return a + b
-    
-result = plus(b=30, a=1)
-print(result)
+    if type(b) is int or type(b) is float:
+        return a + b 
+    else:
+        return None
 
-def say_hello(name, age):
-    return f'Hello {name}. You are {age} years old'
-
-hello = say_hello(24, 'oshmos')
-print(hello)
-hello = say_hello(name='oshmos', age=24)
-print(hello)
+print(plus(12, 1.2))
