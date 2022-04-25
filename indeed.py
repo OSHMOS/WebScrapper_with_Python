aa5d97951c53121899454a8aa69eb3dd49d62133
OSHMOS <ojames9707@gmail.com>
2022-04-25 15:32:29 +0900

    Extracting Locations and Finishing up

--- indeed.py
+++ indeed.py
@@ -19,26 +19,40 @@ def extract_indeed_pages():
     max_page = pages[-1]
     return max_page
 
+def extract_job(html):
+    title = html.find('h2', {'class':'jobTitle'}).find('span').string
+    company = html.find('span', {'class':'companyName'})
+    company_anchor = company.find('a')
+
+    if company_anchor is not None:
+        company = company_anchor.string
+    else:
+        company = company.string
+    # if title == 'new':
+    #     title = title.find_all('span')[1].string
+
+    # if company is not None:
+    #     company = company.string
+    # else:
+    #     company = None
+    location = html.find('div', {'class':'companyLocation'}).text
+    job_id = html['data-jk']
+    return {
+        'title': title, 
+        'company': company, 
+        'location': location,
+        'link': f"https://www.indeed.com/viewjob?jk={job_id}"
+    }
+
 def extract_indeed_jobs(last_page):
     jobs = []
-    # for page in range(last_page):
-    result = requests.get(f'{URL}&start={0*LIMIT}')
-    soup = BeautifulSoup(result.text, 'html.parser')
-    results = soup.find_all('div', {'class': 'fs-unmask'})
-    print('')
-    for result in results:
-        job_title = result.find('h2', {'class':'jobTitle'})
-        title = job_title.find('span').string
-        company = result.find('span', {'class':'companyName'})
-        
-        if title == 'new':
-            title = job_title.find_all('span')[1].string
-
-        if company is not None:
-            company = company.string
-        else:
-            company = None
-            print(title, company)
-
-        print(title, company)
+    for page in range(last_page):
+        print(f'Scrapping page {page}')
+        result = requests.get(f'{URL}&start={page*LIMIT}')
+        soup = BeautifulSoup(result.text, 'html.parser')
+        results = soup.find_all('a', {'class': 'tapItem'})
+        print('')
+        for result in results:
+            job = extract_job(result)
+            jobs.append(job)
     return jobs
