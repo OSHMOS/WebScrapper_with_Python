585f0bf8716afa536252e84f2d84e662d1a028ca
OSHMOS <ojames9707@gmail.com>
2022-04-26 16:08:49 +0900

    Saving to CSV

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from indeed import get_jobs as get_indeed_jobs
-from so import get_jobs as get_so_jobs
 from save import save_to_file
 
 # indeed_jobs = get_indeed_jobs()
-save_to_file(jobs)
+# save_to_file(indeed_jobs)
+save_to_file()
